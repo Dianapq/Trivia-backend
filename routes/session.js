f470c77ab60e04976919541ca61a3bda6f9411f4
Diana Katherine Pinzon <diana.pinzon01@unicatolica.edu.co>
2025-05-11 01:23:32 -0500

    Anexo base de datos Mongo

--- routes/session.js
+++ routes/session.js
@@ -4,14 +4,16 @@ import GameSession from '../models/GameSession.js';
 const router = express.Router();
 
 router.post('/save', async (req, res) => {
+  console.log("📥 Datos recibidos en /save:", req.body);
   try {
     const session = new GameSession(req.body);
     await session.save();
     res.status(201).json({ message: 'Sesión guardada correctamente' });
   } catch (err) {
-    console.error('Error guardando la sesión:', err);
-    res.status(500).json({ error: 'Error al guardar la sesión' });
+    console.error("❌ Error guardando sesión:", err);
+    res.status(500).json({ error: err.message });
   }
 });
+
 
 export default router;
